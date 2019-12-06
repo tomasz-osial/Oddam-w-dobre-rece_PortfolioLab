506bf1b697f1a7d7f0663c6c1096e473bc6edb33
Tomasz Osial <tomasz.osial@gmail.com>
2019-12-06 14:16:13 +0100

    HomeMain

--- my-app/src/components/Home/homeMain.js
+++ my-app/src/components/Home/homeMain.js
@@ -2,50 +2,63 @@ import React from 'react';
 import {NavLink as Link} from "react-router-dom";
 
 import '../../scss/main.scss';
+import decoration from '../../assets/Decoration.svg';
+import icon1 from '../../assets/Icon-1.svg';
+import icon2 from '../../assets/Icon-2.svg';
+import icon3 from '../../assets/Icon-3.svg';
+import icon4 from '../../assets/Icon-4.svg'
 
-const HomeColumns= () => {
+
+const HomeMain = () => {
     return (
-    <div className='homeMainContainer'>
-        <div className='columnsContainer'>
-            <div className='column'>
-                <p className='columnNumber'>
-                    10
-                </p>
-                <span className='columnTitle'>
-                    oddanych worków
-                </span>
-                <p className='columnText'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Deserunt dolorum ducimus ea earum exercitationem laboriosam quia, saepe.
-                </p>
+        <div className='mainContainer'>
+            <div className='mainTitle'>
+                Wystarczą 4 proste kroki
             </div>
-            <div className='column'>
-                <p className='columnNumber'>
-                    5
-                </p>
-                <span className='columnTitle'>
-                    wspartych organizacji
-                </span>
-                <p className='columnText'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Deserunt dolorum ducimus ea earum exercitationem laboriosam quia, saepe.
-                </p>
+            <img src={decoration} alt="decoration" />
+            <div className='mainColumnsContainer'>
+                <div className='mainColumn'>
+                    <img src={icon1} alt="icon1" className='mainIcon'/>
+                    <div className='mainColumnTitle'>
+                        Wybierz rzeczy
+                    </div>
+                    <p className='mainColumnText'>
+                        ubrania, zabawki,<br/>sprzęt i inne
+                    </p>
+                </div>
+                <div className='mainColumn'>
+                    <img src={icon2} alt="icon2" className='mainIcon'/>
+                    <div className='mainColumnTitle'>
+                        Spakuj je
+                    </div>
+                    <p className='mainColumnText'>
+                        skorzystaj z<br/>worków na śmieci
+                    </p>
+                </div>
+                <div className='mainColumn'>
+                    <img src={icon3} alt="icon3" className='mainIcon'/>
+                    <div className='mainColumnTitle'>
+                        Zdecyduj komu<br/>chcesz pomóc
+                    </div>
+                    <p className='mainColumnText'>
+                        wybierz zaufane<br/>miejsce
+                    </p>
+                </div>
+                <div className='mainColumn'>
+                    <img src={icon4} alt="icon4" className='mainIcon'/>
+                    <div className='mainColumnTitle'>
+                        Zamów kuriera
+                    </div>
+                    <p className='mainColumnText'>
+                        kurier przyjedzie<br/>w dogodnym terminie
+                    </p>
+                </div>
             </div>
-            <div className='column'>
-                <p className='columnNumber'>
-                    7
-                </p>
-                <span className='columnTitle'>
-                   zorganizowanych zbiórek
-                </span>
-                <p className='columnText'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Deserunt dolorum ducimus ea earum exercitationem laboriosam quia, saepe.
-                </p>
-            </div>
+            <Link exact to='/logowanie' className='mainActionsContainer'>
+                <div className='mainReturnAction'>oddaj<br/>rzeczy</div>
+            </Link>
         </div>
-    </div>
     )
 };
 
-export default HomeColumns;
+export default HomeMain;
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import '../scss/main.scss';
 import HomeHeader from '../components/Home/homeHeader';
-import HomeColumns from '../components/Home/homeMain'
+import HomeColumns from '../components/Home/homeColumns';
+import HomeMain from '../components/Home/homeMain'
 
 const Home  = () => {
     return (
         <>
             <HomeHeader />
             <HomeColumns />
+            <HomeMain />
         </>
     )
 };
