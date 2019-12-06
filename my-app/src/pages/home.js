9196ff3a0acfaa9f184e9c18de19c870650d855b
Tomasz Osial <tomasz.osial@gmail.com>
2019-12-06 11:26:52 +0100

    HomeColumns

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import '../scss/main.scss';
-import HomeHeader from '../components/Home/homeHeader'
+import HomeHeader from '../components/Home/homeHeader';
+import HomeColumns from '../components/Home/homeMain'
 
 const Home  = () => {
     return (
         <>
             <HomeHeader />
+            <HomeColumns />
         </>
     )
 };
