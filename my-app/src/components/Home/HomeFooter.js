f6837a1d6f39b2e9057ceb409a41cd285bd7fa6a
Tomasz Osial <tomasz.osial@gmail.com>
2019-12-27 18:50:14 +0100

    Form validation improvements

--- my-app/src/components/Home/HomeFooter.js
+++ my-app/src/components/Home/HomeFooter.js
@@ -44,7 +44,7 @@ class Form extends Component {
 
    isFormValid = () => {
        const { name, email, message} = this.state;
-       const isNameValid = name.length > 0;
+       const isNameValid = name.length > 0 && name.indexOf(" ") === -1;
        const validEmailRegex =
            RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
        const isEmailValid = validEmailRegex.test(email);
@@ -60,58 +60,90 @@ class Form extends Component {
        })
    };
    onClickSubmit = event => {
-       // event.preventDefault();
-       const isFormValid = this.isFormValid();
-       if (isFormValid) {
-           this.setState({
-               isFormClicked: true
-           })
+       event.preventDefault();
+       if (this.isFormValid()) {
+            this.sendMessage()
+       } else {
+           console.log("Error");
        }
+   };
+   sendMessage = () => {
+     const URL_ADDRESS = `https://fer-api.coderslab.pl/v1/portfolio/contact`;
+     fetch(URL_ADDRESS, {method: 'POST',
+     headers: {
+         "Content-Type": "application/json"
+     },
+         body: JSON.stringify(this.state)
+     })
+         .then(resp => resp.json())
+         .then(resp => {
+             console.log(resp);
+             if(resp.status === 'success') {
+                 this.setState({
+                     isFormClicked: true
+                 })
+             }
+         })
    };
 
     render() {
+        const { isNameValid, isEmailValid, isMessageValid, isFormClicked } = this.state;
         return (
-            <form
-                className='contactForm'
-                onClick={this.onClickSubmit}
-            >
-                <label>
-                    Wpisz swoje imię<br/>
-                    <input
-                        name='name'
-                        type="text"
-                        placeholder='Twoje imię'
-                        size='25'
-                        onChange={this.onInputChange}
-                    />
-                </label>
-                <label>
-                    Wpisz swój email<br/>
-                    <input
-                        name='email'
-                        type="text"
-                        placeholder='Twój email np. xyz@abc.pl'
-                        size='25'
-                        onChange={this.onInputChange}
-                    />
-                </label>
-                <label>
-                    Wpisz swoją wiadomość
-                    <textarea
-                        name='message'
-                        rows="4" cols="54"
-                        placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing
-                        elit. Nulla varius placerat dolor. Sed venenatis ex aliquam dolor imperdiet placerat.
-                        Curabitur ultricies sollicitudin sapien in bibendum.'
-                        onChange={this.onInputChange}
-                    />
-                </label>
-                <div className='submitContainer'>
-                    <div className='submitBtn'>
-                        Wyślij
+            <>
+            {isFormClicked &&
+            <div className='messageSend'>
+                Wiadomość została wysłana!<br/>Wkrótce się skontaktujemy.
+            </div>}
+                <form
+                    className='contactForm'>
+                    <label>
+                        Wpisz swoje imię<br/>
+                        <input
+                            name='name'
+                            type="text"
+                            placeholder='Twoje imię'
+                            size='25'
+                            onChange={this.onInputChange}
+                        /><br/>
+                        {!isNameValid && <span className='errorInfo'>
+                            Podane imię jest nieprawidłowe!
+                        </span>}
+                    </label>
+                    <label>
+                        Wpisz swój email<br/>
+                        <input
+                            name='email'
+                            type="text"
+                            placeholder='Twój email np. xyz@abc.pl'
+                            size='25'
+                            onChange={this.onInputChange}
+                        /><br/>
+                        {!isEmailValid && <span className='errorInfo'>
+                            Podany email jest nieprawidłowy!
+                        </span>}
+                    </label>
+                    <label>
+                        Wpisz swoją wiadomość
+                        <textarea
+                            name='message'
+                            rows="4" cols="54"
+                            placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Nulla varius placerat dolor. Sed venenatis ex aliquam dolor imperdiet placerat.
+                            Curabitur ultricies sollicitudin sapien in bibendum.'
+                            onChange={this.onInputChange}
+                        /><br/>
+                        {!isMessageValid && <span className='errorInfo'>
+                            Wiadomość musi mieć conajmniej 120 znaków!
+                        </span>}
+                    </label>
+                    <div className='submitContainer'>
+                        <div className='submitBtn'
+                             onClick={this.onClickSubmit}>
+                            Wyślij
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </>
         )
     }
 }
