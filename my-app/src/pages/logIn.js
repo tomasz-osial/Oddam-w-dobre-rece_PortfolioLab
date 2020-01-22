1eca31f643941541984a7d7823bb96a81db8cba9
Tomasz Osial <tomasz.osial@gmail.com>
2020-01-22 17:12:16 +0100

    Login form validation

--- my-app/src/pages/logIn.js
+++ my-app/src/pages/logIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 
 import '../scss/main.scss';
@@ -52,31 +52,94 @@ const LogIn  = () => {
                     Zaloguj się
                 </div>
                 <img src={decoration} alt='decoration'/>
+                <LogInForm />
+            </div>
+        </div>
+    )
+};
+
+class LogInForm extends Component {
+
+    state = {
+     email: '',
+     password: '',
+     isEmailValid: true,
+     isPasswordValid: true,
+    };
+
+    isFormValid = () => {
+      const { email, password } = this.state;
+        const validEmailRegex =
+            RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+      const isEmailValid = validEmailRegex.test(email);
+      const isPasswordValid = password.length >= 6;
+      this.setState({
+          isEmailValid, isPasswordValid
+      });
+      return isEmailValid && isPasswordValid
+    };
+
+    onInputChange  = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    };
+
+    onClickLogIn = event => {
+        event.preventDefault();
+        if (this.isFormValid()) {
+            console.log("Zalogowano")
+        } else {
+            console.log("Sprawdź dane")
+        }
+    };
+
+    render() {
+        const { isEmailValid, isPasswordValid } = this.state;
+        return (
+            <>
                 <form className='logForm'>
-                    <label>
-                        Email<br/>
-                        <input type="text"/>
-                        <br/>
-                    </label>
-                    <label>
-                        Hasło<br/>
-                        <input type="password"/>
-                        <br/>
-                    </label>
-                </form>
+                <label>
+                    Email<br/>
+                    <input
+                        type="text"
+                        name='email'
+                        onChange={this.onInputChange}
+                        className={this.state.isEmailValid === false ? 'formError' : 'logFormInput'}
+                    />
+                    <br/>
+                    {!isEmailValid && <span className='errorInfo'>
+                        Podany email jest nieprawidłowy!
+                    </span>}
+                </label>
+                <label>
+                    Hasło<br/>
+                    <input
+                        type="password"
+                        name='password'
+                        onChange={this.onInputChange}
+                        className={this.state.isPasswordValid === false ? 'formError' : 'logFormInput'}
+                    />
+                    <br/>
+                    {!isPasswordValid && <span className='errorInfo'>
+                        Podany hasło jest za krótkie!
+                    </span>}
+                </label>
+            </form>
                 <div className='regActionContainer'>
                     <NavLink exact to = '/rejestracja'>
                         <div className='logRegister'>
                             Załóż konto
                         </div>
                     </NavLink>
-                    <div className='logLogin'>
+                    <div className='logLogin'
+                        onClick={this.onClickLogIn}>
                         Zaloguj się
                     </div>
                 </div>
-            </div>
-        </div>
-    )
-};
+                </>
+        )
+    }
+}
 
 export default LogIn;
