e4cebbc2a159ac2a601bd92a69a49b6457830852
Tomasz Osial <tomasz.osial@gmail.com>
2020-02-01 17:54:35 +0100

    Updates

---
+++ precache-manifest.09dcbd90bf6e483215add3b935e79545.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62eda0491e39fd8e738a0024057a6763",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/index.html"
+  },
+  {
+    "revision": "5091117399b45e152ba8",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/css/main.ffb30cfc.chunk.css"
+  },
+  {
+    "revision": "fed597d5de24122b6cee",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/js/2.be00ed99.chunk.js"
+  },
+  {
+    "revision": "5091117399b45e152ba8",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/js/main.ebb33fcf.chunk.js"
+  },
+  {
+    "revision": "bfe48594a61f2e472946",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/js/runtime-main.6934e04e.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddam-w-dobre-rece_PortfolioLab/static/media/Signature.e54cfcfc.svg"
+  }
+]);
